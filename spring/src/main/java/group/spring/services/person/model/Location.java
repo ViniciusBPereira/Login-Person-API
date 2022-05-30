f1395da2ecdd01920c2759438817360cf329947f
viniciusbpereira <viniciusbegosso@gmail.com>
2022-05-30 15:08:02 -0300

    Model for ViaCEP

---
+++ spring/src/main/java/group/spring/services/person/model/Location.java
@@ -0,0 +1,30 @@
+package group.spring.services.person.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+   	private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+}
